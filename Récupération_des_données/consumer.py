7c7143f30d1b1030b3737c2f29086e7e511ae0da
keagnon <keagnon.gbe@gmail.com>
2023-02-25 21:46:04 +0100

    Struturation du projet

--- Récupération_des_données/consumer.py
+++ Récupération_des_données/consumer.py
@@ -4,7 +4,7 @@ import json
 # Définir les informations de connexion Kafka
 KAFKA_TOPIC = 'raw_velib_data'
 
-# Créer un consumer Kafka
+# Créer un consumer Kafka'""
 consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers='localhost:9092')
 
 # bouclez pour consommer les messages du topic Kafkaa
